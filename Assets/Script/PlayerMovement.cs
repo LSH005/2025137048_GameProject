e0393c90214da8ec90ccf658297cf8bef6da5ad5
LSH005 <>
2025-03-31 15:51:08 +0900

    [Feet] 유니티 충돌 처리 함수 사용

--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -9,6 +9,8 @@ public class PlayerMovement : MonoBehaviour
     public Rigidbody rb;    //플레이어 강체 선언
     public float jumpForce = 5.0f;  //점프 첨 값 주기
     public bool isGround = true;    //땅에 있는지 감지
+    public int coinCount = 0; // 코인 갯수
+    public int totalCoins = 5; //요구하는 코인 갯수
 
     // Start is called before the first frame update
     void Start()
@@ -35,6 +37,18 @@ public class PlayerMovement : MonoBehaviour
         if (collision.gameObject.tag == "Ground")
         {    //충돌이 일어난 물체의 태그가 "Ground" 인가 ?
             isGround = true;   //땅에 부딛히면 다시 점프 권한 true
+        }
+    }
+    void OnTriggerEnter(Collider other) // 트리거 영역 안쪽인가를 감지
+    {
+        if (other.CompareTag("Coin"))
+        {
+            coinCount++; // 코인 변수 1 증가 (++ 은 1 증가)
+            Destroy(other.gameObject); // 코인 삭제.
+            Debug.Log($"코인 수집 : {coinCount} / {totalCoins}"); //수집 코인 갯수 분수 형태로 출력 (변수 매크로)
+        }
+        if (other.CompareTag("Door")&& coinCount >= totalCoins){
+            Debug.Log("게임 클리어"); // 이후 종료 연출 및 Scene 전환
         }
     }
 }
